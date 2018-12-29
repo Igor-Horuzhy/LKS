81ed56d56b3a2c4dc06fd0ed2db54c90e0f792d8
Igor.Horuzhiy <igor.horuzhiy@gmail.com>
2018-12-29 16:21:52 +0300

    laba 6 init

---
+++ laba6/src/index.js
@@ -0,0 +1,67 @@
+import timer from './timer.js';
+import './styles.css';
+
+function component() {
+  var element = document.createElement('div');
+  var br      = document.createElement('br');
+  var span    = document.createElement('span');
+  span.innerHTML = 'Remaining seconds: '
+  element.setAttribute('class','parent');
+
+  var secondsToShow=document.createElement('div');
+  var timerSec= document.createElement('label');
+  var timeInput=document.createElement('input'); 
+  timeInput.setAttribute('type','number');
+  timeInput.setAttribute('placeholder','Seconds');
+  timeInput.setAttribute('id','seconds');
+  secondsToShow.setAttribute('id','secondsToShow');
+  secondsToShow.innerHTML=0;
+  timerSec.setAttribute('for','seconds');
+  
+  var text= document.createElement('label');
+  var textInput=document.createElement('input');
+  textInput.setAttribute('placeholder','Text for alert');
+  textInput.setAttribute('id','text');
+  
+  var startButton=document.createElement('button');
+  var resetButton=document.createElement('button');
+  var pauseButton=document.createElement('button');
+  var stopButton=document.createElement('button');
+    
+
+  startButton.setAttribute('id','start'); 
+  startButton.setAttribute('class','button');
+  startButton.innerHTML='Start';
+
+  resetButton.setAttribute('id','reset');
+  resetButton.setAttribute('class','button');
+  resetButton.innerHTML='Reset';
+
+  pauseButton.setAttribute('id','pause');
+  pauseButton.setAttribute('class','button');
+  pauseButton.innerHTML='Pause';
+
+  stopButton.setAttribute('id','stop');
+  stopButton.setAttribute('class','button');
+  stopButton.innerHTML='Stop';
+  
+  
+
+  element.appendChild(timerSec);  
+  timerSec.appendChild(timeInput);
+  text.appendChild(textInput);   
+  element.appendChild(text);
+  element.appendChild(br);
+  element.appendChild(startButton); 
+  element.appendChild(resetButton);
+  element.appendChild(pauseButton);
+  element.appendChild(stopButton);
+  element.appendChild(br);
+  element.appendChild(span);  
+  element.appendChild(secondsToShow);
+  
+  return element;
+}
+
+var element = component();
+document.body.appendChild(element);
---
+++ laba6/src/timer.js
@@ -0,0 +1,89 @@
+class Timer {
+  constructor(inputSecs,callback) {
+    this.paused  = false;
+    this.started = false;
+    this.stopped = true;
+    this.counter = 0 ;
+    this.running = false;
+    this.inputSecs = inputSecs;
+    this.callback=callback;
+    this.inputText = document.getElementById("text");
+  }
+
+  start() {
+    if (this.stopped) {
+      this.started = true;
+      this.paused = false;
+      this.stopped = false;
+      this.counter = Number(this.inputSecs.value);
+      this.loop();
+      this.running = true;
+    }
+
+    this.paused = false;
+  }
+
+  pause() {
+    this.paused = true;
+  }
+
+  stop() {
+    this.paused = false;
+    this.started = false;
+    this.stopped = true;
+    this.running = false;
+    this.counter = 0;
+  }
+
+  update(item) {
+    document.getElementById("secondsToShow").innerHTML = item;    
+    --this.counter;
+
+    if (this.counter < 0) {
+      this.callback.call();      
+      this.stop();
+    }
+  }
+
+  reset() {
+    this.counter = Number(this.inputSecs.value);
+
+    document.getElementById("secondsToShow").innerHTML = this.counter;
+  }
+
+  loop() {
+    if (!this.stopped) {
+      if (!this.paused) {
+        this.update(this.counter);
+      }
+
+      setTimeout(() => this.loop(), 1000);
+    }
+  }
+}
+
+
+function alertText(){
+  var inputText = document.getElementById("text");
+  var word = inputText.value;
+  alert(word);
+}
+
+window.onload = function() {  
+
+  let inputSecs = document.getElementById("seconds");
+  const timer = new Timer(inputSecs,alertText);
+  document.getElementById("start").onclick = function() {
+    timer.start();
+  }
+  document.getElementById("reset").onclick = function() { 
+    return timer.reset(); 
+  }
+  document.getElementById("pause").onclick = function() { 
+    return timer.pause();
+  }
+  document.getElementById("stop").onclick = function()  
+  { 
+    return timer.stop(); 
+  }
+};
